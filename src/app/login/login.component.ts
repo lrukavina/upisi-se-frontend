5cdc3f6a67381b5c70c2c43e0b1c880fa69d0c10
lrukavina <77284546+lrukavina@users.noreply.github.com>
2023-11-16 17:48:00 +0100

    implementirana prijava
    * Implementacija servisne logike za prijavu korisnika u aplikaciju

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,4 +1,6 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
+import {LoginService} from "./login.service";
+import {Router} from "@angular/router";
 
 @Component({
   selector: 'app-login',
@@ -7,4 +9,21 @@ import { Component } from '@angular/core';
 })
 export class LoginComponent {
 
+  constructor(
+    private loginService: LoginService,
+    private route: Router
+  ) {
+  }
+
+  prijavaKorisnika(korisnickoIme: string, lozinka: string): void {
+    korisnickoIme = korisnickoIme.trim();
+    lozinka = lozinka.trim();
+
+    this.loginService.autentikacijaKorisnika({korisnickoIme: korisnickoIme, lozinka: lozinka}).subscribe(token => {
+      sessionStorage.setItem('token', token.token);
+      this.route.navigate(['/pregled-upisa']);
+    }, error => {
+      console.log(error);
+    });
+  }
 }
