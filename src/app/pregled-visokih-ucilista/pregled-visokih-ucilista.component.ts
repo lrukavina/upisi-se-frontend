cc83a8c64620a1fefbc924a15c33815a6c767ff1
lrukavina <luka.rukavina8@gmail.com>
2023-11-18 21:22:48 +0100

    pretraga visokih ucilista

--- src/app/pregled-visokih-ucilista/pregled-visokih-ucilista.component.ts
+++ src/app/pregled-visokih-ucilista/pregled-visokih-ucilista.component.ts
@@ -1,6 +1,7 @@
 import {Component, OnInit} from '@angular/core';
 import {VisokoUciliste} from "../common/visokoUciliste";
 import {PregledVisokihUcilistaService} from "./pregled-visokih-ucilista.service";
+import {Student} from "../common/student";
 
 @Component({
   selector: 'app-pregled-visokih-ucilista',
@@ -12,6 +13,7 @@ export class PregledVisokihUcilistaComponent implements OnInit {
   modalOtvoren = false;
 
   visokaUcilista: VisokoUciliste[] = [];
+  visokaUcilistaPrikaz: VisokoUciliste[] = [];
 
   constructor(private pregledVisokihUcilistaService: PregledVisokihUcilistaService) {
   }
@@ -28,5 +30,19 @@ export class PregledVisokihUcilistaComponent implements OnInit {
       }, error => {
         console.log(error);
       });
+  }
+
+  pretrazi(): void {
+    let inputPolje = document.getElementById('pretraga') as HTMLInputElement;
+    let str = inputPolje.value;
+
+    this.visokaUcilistaPrikaz = this.visokaUcilista;
+
+    this.visokaUcilistaPrikaz = this.visokaUcilista.filter(visokoUciliste =>
+      visokoUciliste.naziv.toLowerCase().includes(str.toLowerCase()) ||
+      visokoUciliste.adresa.toLowerCase().includes(str.toLowerCase()) ||
+      visokoUciliste.postanskiBroj.toLowerCase().includes(str.toLowerCase()) ||
+      visokoUciliste.mjesto.toLowerCase().includes(str.toLowerCase())
+    );
   }
 }
